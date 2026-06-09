bbc4bc4442da00857d82d91748affc4aa26d76b9
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-06-09 22:45:37 +0530

    config ini fix

--- PY_pytest/TutuorialNinja_DataDriven/read_config.py
+++ PY_pytest/TutuorialNinja_DataDriven/read_config.py
@@ -2,5 +2,5 @@ from configparser import ConfigParser
 
 def get_config(category, key):
     config = ConfigParser()
-    config.read("./config.ini")
+    config.read("D:\python selenium-D\PY_pytest\TutuorialNinja_DataDriven\config.ini")
     return config.get(category, key)
